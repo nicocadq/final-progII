258dcb6e0f452472d07634e4e5300c8909ad4263
alejandrogon1418 <56725406+alejandrogon1418@users.noreply.github.com>
2020-08-20 10:15:43 -0300

    Created all the classes withh the methods without the class called Exmans

--- final-progII/src/logic/ControllerLogic.java
+++ final-progII/src/logic/ControllerLogic.java
@@ -2,4 +2,51 @@ package logic;
 
 public class ControllerLogic {
 
-}
+	public User createUsers(User user) {
+
+		return user;
+	}
+
+	public Subject createSubject(Subject subject) {
+
+		return subject;
+	}
+
+	public Absence createSubject(Absence absence) {
+
+		return absence	;
+	}
+	
+	public User consultUsers (String ci) {
+		return null;
+		//return Usuario.ci o alg asi
+	}
+	
+	public Subject consultSubject (String code) {
+		return null;
+		//return Materi.codigo
+	}
+	
+	public ArrayList<Abstence> consultAbstence (String ci){
+		return null;
+		//...?
+	}
+	public ArrayList<Student> studentsToList () {
+		return null;
+		//void creo
+	}
+	public ArrayList<Subject> subjectsToList (){
+		return null;
+	}
+	public ArrayList<Abstence> abstencesToList (){
+		return null;
+	}
+	public ArrayList<Abstence> abstencesToList (LocalDate ){
+		return null;
+	}
+
+	
+	
+	
+	
+	}

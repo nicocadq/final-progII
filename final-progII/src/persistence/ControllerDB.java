d7af987adb8a4cb0d7c66d9853027174e9403a0d
nicocadq <nmachado.camejo@gmail.com>
2020-08-20 11:22:45 -0300

    Added ControllerDB methods signature

---
+++ final-progII/src/persistence/ControllerDB.java
@@ -0,0 +1,42 @@
+package persistence;
+
+import java.util.ArrayList;
+
+public class ControllerDB {
+
+	public void toPersistUser(User user) {
+
+	}
+
+	public void toPersistSubject(Subject subject) {
+
+	}
+	
+	public void toPersistAbsence(Absence absence) {
+		
+	}
+
+	public void toPersistExam(Exam exam) {
+		
+	}
+	
+	public User recoverUser(String ci) {
+		
+	}
+	
+	public Subject recoverSubject(String code) {
+		
+	}
+	
+	public ArrayList<User> recoverUsers(){
+		
+	}
+	
+	public ArrayList<Subject> recoverSubjects(){
+		
+	}
+	
+	public ArrayList<Subject> recoverExams(){
+		
+	}
+}

a3f36fe34ecfce8c10516e9556b2700e060d3f53
nicocadq <nmachado.camejo@gmail.com>
2020-10-06 12:15:27 -0300

    Implemented recoverUser()

--- final-progII/src/persistence/ControllerDB.java
+++ final-progII/src/persistence/ControllerDB.java
@@ -10,6 +10,9 @@ import javax.security.auth.Subject;
 import logic.Absence;
 import logic.Exam;
 import logic.Functionary;
+import logic.Generation;
+import logic.Orientation;
+import logic.Status;
 import logic.Student;
 import logic.Teacher;
 import logic.User;
@@ -40,14 +43,14 @@ public class ControllerDB extends Conn {
 			System.out.println("Update rows: " + updatedUserRows);
 
 			try {
-				
+
 				System.out.println("Casting User to Student");
 				Student student = (Student) user;
 
 				System.out.println("Creating PreparedStatement");
 
-				PreparedStatement studentSt = this.conn
-						.prepareStatement("INSERT INTO Student(CIUSER, ORIENTATION, GENERATION, STATE) VALUES(?, ?, ? ,?)");
+				PreparedStatement studentSt = this.conn.prepareStatement(
+						"INSERT INTO Student(CIUSER, ORIENTATION, GENERATION, STATE) VALUES(?, ?, ? ,?)");
 
 				studentSt.setInt(1, student.getCi());
 				studentSt.setString(2, student.getOrientation().toString());
@@ -63,19 +66,18 @@ public class ControllerDB extends Conn {
 				System.out.println("Update rows: " + updatedStudentRows);
 
 			} catch (ClassCastException castStudentEx) {
-				
+
 				try {
-					
+
 					System.out.println("Casting User to Teacher");
 					Teacher teacher = (Teacher) user;
 
 					System.out.println("Creating PreparedStatement");
 
-					PreparedStatement teacherSt = this.conn
-							.prepareStatement("INSERT INTO Teacher(CIUSER) VALUES(?)");
+					PreparedStatement teacherSt = this.conn.prepareStatement("INSERT INTO Teacher(CIUSER) VALUES(?)");
 
 					teacherSt.setInt(1, teacher.getCi());
-					
+
 					System.out.println("Created PreparedStatement.");
 
 					System.out.println("Execute Update.");
@@ -85,9 +87,9 @@ public class ControllerDB extends Conn {
 					System.out.println("Update rows: " + updatedteacherRows);
 
 				} catch (ClassCastException castTeacherEx) {
-					
+
 					try {
-						
+
 						System.out.println("Casting User to Functionary");
 						Functionary functionary = (Functionary) user;
 
@@ -97,7 +99,7 @@ public class ControllerDB extends Conn {
 								.prepareStatement("INSERT INTO Functionary(CIUSER) VALUES(?)");
 
 						functionarySt.setInt(1, functionary.getCi());
-						
+
 						System.out.println("Created PreparedStatement.");
 
 						System.out.println("Execute Update.");
@@ -134,8 +136,104 @@ public class ControllerDB extends Conn {
 
 	}
 
-	public User recoverUser(String ci) {
-		return null;
+	public User recoverUser(int ci) throws Exception {
+
+		User user = null;
+
+		try {
+			System.out.println("Creating a Connection Object.");
+			this.MySQLconnection();
+
+			System.out.println("Creating PreparedStatement");
+			PreparedStatement userSt = this.conn.prepareStatement("SELECT * FROM User WHERE CI = ?");
+
+			userSt.setInt(1, ci);
+
+			System.out.println("Executing Query and creating ResultSet.");
+			ResultSet userRs = userSt.executeQuery();
+
+			while (userRs.next()) {
+
+				System.out.println("Database User");
+
+				user = new User(userRs.getInt("CI"), userRs.getString("NAME"), userRs.getString("LASTNAME"),
+						userRs.getString("MAIL"), userRs.getString("PASSWORD"), userRs.getDate("BIRTH").toLocalDate());
+			}
+
+			if (user != null) {
+				try {
+
+					System.out.println("Creating PreparedStatement");
+					PreparedStatement studentSt = this.conn.prepareStatement("SELECT * FROM Student WHERE CI = ?");
+
+					userSt.setInt(1, user.getCi());
+
+					System.out.println("Executing Query and creating ResultSet.");
+					ResultSet studentRs = studentSt.executeQuery();
+
+					while (studentRs.next()) {
+
+						System.out.println("Database Student");
+
+						user = new Student(userRs.getInt("CI"), userRs.getString("NAME"), userRs.getString("LASTNAME"),
+								Orientation.valueOf(studentRs.getString("ORIENTATION")),
+								Status.valueOf(studentRs.getString("STATUS")),
+								Generation.valueOf(studentRs.getString("GENERATION")), userRs.getString("MAIL"),
+								userRs.getString("PASSWORD"), userRs.getDate("BIRTH").toLocalDate());
+					}
+
+				} catch (Exception studentEx) {
+					try {
+
+						System.out.println("Creating PreparedStatement");
+						PreparedStatement teacherSt = this.conn.prepareStatement("SELECT * FROM Teacher WHERE CI = ?");
+
+						userSt.setInt(1, user.getCi());
+
+						System.out.println("Executing Query and creating ResultSet.");
+						ResultSet teacherRs = teacherSt.executeQuery();
+
+						while (teacherRs.next()) {
+
+							System.out.println("Database Teacher");
+
+							user = new Teacher(userRs.getInt("CI"), userRs.getString("NAME"),
+									userRs.getString("LASTNAME"), userRs.getString("MAIL"),
+									userRs.getString("PASSWORD"), userRs.getDate("BIRTH").toLocalDate());
+						}
+
+					} catch (Exception teacherEx) {
+						try {
+
+							System.out.println("Creating PreparedStatement");
+							PreparedStatement functionarySt = this.conn.prepareStatement("SELECT * FROM Functionary WHERE CI = ?");
+
+							userSt.setInt(1, user.getCi());
+
+							System.out.println("Executing Query and creating ResultSet.");
+							ResultSet functionaryRs = functionarySt.executeQuery();
+
+							while (functionaryRs.next()) {
+
+								System.out.println("Database Functioanry");
+
+								user = new Functionary(userRs.getInt("CI"), userRs.getString("NAME"),
+										userRs.getString("LASTNAME"), userRs.getString("MAIL"),
+										userRs.getString("PASSWORD"), userRs.getDate("BIRTH").toLocalDate());
+							}
+
+						} catch (Exception functionaryEx) {
+
+						}
+					}
+				}
+			}
+
+		} catch (Exception ex) {
+			throw ex;
+		}
+
+		return user;
 
 	}
 

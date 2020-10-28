7b345419614da9efb64b9551102649ea28f7ebac
alejandrogon1418 <56725406+alejandrogon1418@users.noreply.github.com>
2020-10-28 08:49:04 -0300

    Some changes to the JMenu

--- final-progII/src/presentation/Menu.java
+++ final-progII/src/presentation/Menu.java
@@ -16,15 +16,48 @@ public class Menu {
 		menuBar.setBounds(0, 0, 230, 22);
 		panel.add(menuBar, BorderLayout.NORTH);
 
-		JMenu menu = new JMenu("Menu");
-		menuBar.add(menu);
+		JMenu menu_User = new JMenu("Users");
+		JMenu menu_Subject = new JMenu("Subject");
+		JMenu menu_Absence = new JMenu("Absence");
+		JMenu menu_Exam = new JMenu("Exam");
+		JMenu menu_Exit = new JMenu("Exit");
+		JMenu menu_Space = new JMenu("                                                                               ");
+		JMenu menu_LogOut = new JMenu("Log Out");
 
+		menuBar.add(menu_User);
+		menuBar.add(menu_Subject);
+		menuBar.add(menu_Absence);
+		menuBar.add(menu_Exam);
+		menuBar.add(menu_Exit);
+		menuBar.add(menu_Space);
+		menuBar.add(menu_LogOut);
+
+		/*Sub Menu of the JMenu Users*/
 		JMenuItem createUser__menuItem = new JMenuItem("Create New User");
-		menu.add(createUser__menuItem);
+		menu_User.add(createUser__menuItem);
+		
+		JMenuItem consultUsers_menuItem = new JMenuItem("Consult Users");
+		menu_User.add(consultUsers_menuItem);
+		
+		JMenuItem listUsers_menuItem = new JMenuItem("List Users");
+		menu_User.add(listUsers_menuItem);
+		
+		/*Sub Menu of the JMenu Subject*/
+		JMenuItem consultSubject__menuItem = new JMenuItem("Consult Subjects");
+		menu_Subject.add(consultSubject__menuItem);
+		
+		JMenuItem consultSubject_menuItem = new JMenuItem("List Subjects");
+		menu_Subject.add(consultSubject_menuItem);
+		
+		JMenuItem deleteSubject_menuItem = new JMenuItem("Create Subject");
+		menu_Subject.add(deleteSubject_menuItem);
 
+		
+		
 		createUser__menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				master__cardLayout.show(master__panel, "CREATE_USER_PANEL");
+				
 			}
 		});
 	}

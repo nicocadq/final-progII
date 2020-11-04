02ffe694d2aa9c0ede9533f97b6c7cb2d0ec13db
nicocadq <67390791+nicocadq@users.noreply.github.com>
2020-11-03 22:32:07 -0300

    Update SCRIPT.sql

--- SCRIPT.sql
+++ SCRIPT.sql
@@ -14,12 +14,10 @@ CREATE TABLE Subject( idSubject VARCHAR(64), name VARCHAR(64), orientation VARCH
 
 CREATE TABLE Student( ciUser INT NOT NULL, orientation VARCHAR(64), generation VARCHAR(64), state VARCHAR(64), PRIMARY KEY (ciUser), FOREIGN KEY (ciUser) REFERENCES User(ci));
 
-CREATE TABLE Exam( idExam INT NOT NULL AUTO_INCREMENT, ciStudent INT NOT NULL, idSubject VARCHAR(64), date DATE, PRIMARY KEY (idExam, ciStudent), FOREIGN KEY (idSubject) REFERENCES Subject(idSubject), FOREIGN KEY (ciStudent) REFERENCES Student(ciUser));
+CREATE TABLE Exam( idExam INT NOT NULL AUTO_INCREMENT, ciStudent INT NOT NULL, idSubject VARCHAR(64), date DATE, markFailed INT, PRIMARY KEY (idExam, ciStudent), FOREIGN KEY (idSubject) REFERENCES Subject(idSubject), FOREIGN KEY (ciStudent) REFERENCES Student(ciUser));
 
 CREATE TABLE Absence(idAbsence INT NOT NULL AUTO_INCREMENT, ciStudent INT NOT NULL, idSubject VARCHAR(64) , type VARCHAR(64), date DATE, PRIMARY KEY (idAbsence, ciStudent), FOREIGN KEY (ciStudent) REFERENCES Student(ciUser), FOREIGN KEY (idSubject) REFERENCES Subject(idSubject));
 
 CREATE TABLE Takes(ciStudent INT NOT NULL, idSubject VARCHAR(64), PRIMARY KEY (ciStudent, idSubject), FOREIGN KEY (ciStudent) REFERENCES Student(ciUser), FOREIGN KEY (idSubject) REFERENCES Subject(idSubject));
-
-CREATE TABLE Took(idExam INT NOT NULL, ciStudent INT NOT NULL, markFailed INT, PRIMARY KEY(idExam, ciStudent), FOREIGN KEY (idExam) REFERENCES Exam(idExam), FOREIGN KEY (ciStudent) REFERENCES Student(ciUser));
 
 CREATE TABLE Teaches(idSubject VARCHAR(64), ciTeacher INT, PRIMARY KEY (idSubject), FOREIGN KEY (idSubject) REFERENCES Subject(idSubject), FOREIGN KEY (ciTeacher) REFERENCES Teacher(ciUser));
